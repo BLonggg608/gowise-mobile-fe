e8a607c35e15f1510b1997251297558ca156720a
BLonggg <tranhabaolong608@gmail.com>
2025-09-10 22:01:44 +0700

    init

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,12 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  useFonts({
+    "inter-bold": require("../assets/fonts/Inter_18pt-Bold.ttf"),
+    "inter-medium": require("../assets/fonts/Inter_18pt-Medium.ttf"),
+    "inter-regular": require("../assets/fonts/Inter_18pt-Regular.ttf"),
+  });
+
+  return <Stack screenOptions={{ headerShown: false }} />;
 }
---
+++ constant/Colors.ts
@@ -0,0 +1,33 @@
+/**
+ * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
+ * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
+ */
+
+const tintColorLight = "#0a7ea4";
+const tintColorDark = "#fff";
+
+export const Colors = {
+  LIGHT_GREEN: "#10B981",
+  GREEN: "#0D9488",
+  BLACK: "#1E293B",
+  GRAY: "#64748B",
+  WHITE: "#FFFFFF",
+  YELLOW: "#F59E0B",
+  RED: "#EF4444",
+  light: {
+    text: "#11181C",
+    background: "#fff",
+    tint: tintColorLight,
+    icon: "#687076",
+    tabIconDefault: "#687076",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#ECEDEE",
+    background: "#151718",
+    tint: tintColorDark,
+    icon: "#9BA1A6",
+    tabIconDefault: "#9BA1A6",
+    tabIconSelected: tintColorDark,
+  },
+};
